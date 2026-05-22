755ce5ee8b6e4ea346dfeee7dfaa3ce1432a96a4
Song <sl5807@columbia.edu>
2026-05-22 17:02:50 -0400

    Reduce chat app bar blur by 50%

--- lib/Widgets/chat_app_bar.dart
+++ lib/Widgets/chat_app_bar.dart
@@ -100,7 +100,7 @@ class ChatAppBar extends StatelessWidget implements PreferredSizeWidget {
       scrolledUnderElevation: 0,
       flexibleSpace: ClipRect(
         child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+          filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
           child: Container(color: Colors.transparent),
         ),
       ),

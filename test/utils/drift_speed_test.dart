fa9fd68f2b2f43e881e374bf51c16fc4f283d983
Song <sl5807@columbia.edu>
2026-06-16 22:06:03 -0700

    Add generation-reactive drift speed model

---
+++ test/utils/drift_speed_test.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:llamaseek/Utils/drift_speed.dart';
+
+void main() {
+  group('targetDriftSpeed', () {
+    test('rest speed when idle', () {
+      expect(targetDriftSpeed(isGenerating: false), kRestDriftSpeed);
+    });
+    test('faster when generating', () {
+      expect(targetDriftSpeed(isGenerating: true), kGeneratingDriftSpeed);
+      expect(kGeneratingDriftSpeed, greaterThan(kRestDriftSpeed));
+    });
+  });
+
+  group('easeDriftSpeed', () {
+    test('returns current unchanged for non-positive dt', () {
+      expect(easeDriftSpeed(1.0, 1.4, 0.0), 1.0);
+      expect(easeDriftSpeed(1.0, 1.4, -0.5), 1.0);
+    });
+
+    test('moves toward target without overshoot', () {
+      final next = easeDriftSpeed(1.0, 1.4, 0.016);
+      expect(next, greaterThan(1.0));
+      expect(next, lessThan(1.4));
+    });
+
+    test('eases back down toward rest', () {
+      final next = easeDriftSpeed(1.4, 1.0, 0.016);
+      expect(next, lessThan(1.4));
+      expect(next, greaterThan(1.0));
+    });
+
+    test('converges to target after enough steps', () {
+      var s = 1.0;
+      for (var i = 0; i < 600; i++) {
+        s = easeDriftSpeed(s, 1.4, 0.016);
+      }
+      expect(s, closeTo(1.4, 0.001));
+    });
+  });
+}

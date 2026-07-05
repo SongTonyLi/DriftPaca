2d330ec622001490570f55a1a95a087e85f7d9c1
Song <sl5807@columbia.edu>
2026-07-05 08:29:33 -0700

    Add span tests for multi-message replies and consecutive user turns

--- test/services/exchange_span_test.dart
+++ test/services/exchange_span_test.dart
@@ -4,6 +4,7 @@ import 'package:llamaseek/Providers/chat_provider.dart';
 
 OllamaMessage _u(String c) => OllamaMessage(c, role: OllamaMessageRole.user);
 OllamaMessage _a(String c) => OllamaMessage(c, role: OllamaMessageRole.assistant);
+OllamaMessage _s(String c) => OllamaMessage(c, role: OllamaMessageRole.system);
 
 void main() {
   group('computeExchangeSpan', () {
@@ -39,6 +40,27 @@ void main() {
       final m = [_u('q1'), _a('a1')];
       final span = ChatProvider.computeExchangeSpan(m, _u('not in list'));
       expect(span.end - span.start, 0);
+    });
+
+    test('multi-message reply run is fully covered from the user anchor', () {
+      final m = [_u('q1'), _s('sys'), _a('a1'), _u('q2')];
+      final span = ChatProvider.computeExchangeSpan(m, m[0]);
+      expect(span.start, 0);
+      expect(span.end, 3);
+    });
+
+    test('assistant anchor in a multi-message run walks back to its user', () {
+      final m = [_u('q1'), _s('sys'), _a('a1'), _u('q2')];
+      final span = ChatProvider.computeExchangeSpan(m, m[2]);
+      expect(span.start, 0);
+      expect(span.end, 3);
+    });
+
+    test('consecutive user messages: the first selects only itself', () {
+      final m = [_u('u1'), _u('u2'), _a('a2')];
+      final span = ChatProvider.computeExchangeSpan(m, m[0]);
+      expect(span.start, 0);
+      expect(span.end, 1);
     });
   });
 }
